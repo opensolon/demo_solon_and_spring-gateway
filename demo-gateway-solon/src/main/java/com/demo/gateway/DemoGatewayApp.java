a13c8808a67e47ea84d451297e71f533bc324621
noear <noear@live.cn>
2024-08-17 18:49:43 +0800

    demo

---
+++ demo-gateway-solon/src/main/java/com/demo/gateway/DemoGatewayApp.java
@@ -0,0 +1,10 @@
+package com.demo.gateway;
+
+
+import org.noear.solon.Solon;
+
+public class DemoGatewayApp {
+    public static void main(String[] args) {
+        Solon.start(DemoGatewayApp.class, args);
+    }
+}
